75c79bf5564abccdf39af9454f6b8419aa77d883
yurirnascimento <yuri.rsl@gmail.com>
2022-01-28 16:19:13 -0300

    Update HasResponseClass.php
    Fix small thinks

--- src/Traits/Features/HasResponseClass.php
+++ src/Traits/Features/HasResponseClass.php
@@ -3,6 +3,8 @@
 namespace Sammyjo20\Saloon\Traits\Features;
 
 use Sammyjo20\Saloon\Exceptions\SaloonInvalidResponseClassException;
+use Sammyjo20\Saloon\Exceptions\SaloonInvalidConnectorException;
+use Sammyjo20\Saloon\Http\SaloonRequest;
 use Sammyjo20\Saloon\Http\SaloonResponse;
 use \ReflectionClass;
 
@@ -16,6 +18,7 @@ trait HasResponseClass
 	 *
 	 * @return string
 	 * @throws \ReflectionException
+	 * @throws SaloonInvalidConnectorException
 	 * @throws SaloonInvalidResponseClassException
 	 */
 	public function getResponseClass() : string
@@ -23,14 +26,14 @@ trait HasResponseClass
 		$response = $this->response;
 
 		if(!$response) {
-			$response = method_exists($this, "getConnector") ? $this->getConnector()->getResponseClass() : SaloonResponse::class;
+			$response = $this instanceof SaloonRequest ? $this->getConnector()->getResponseClass() : SaloonResponse::class;
 		}
 
 		if(!class_exists($response)) {
 			throw new SaloonInvalidResponseClassException;
 		}
 
-		$isValidResponse = $response == SaloonResponse::class
+		$isValidResponse = $response === SaloonResponse::class
 			|| (new ReflectionClass($response))->isSubclassOf(SaloonResponse::class);
 
 		if(!$isValidResponse) {
