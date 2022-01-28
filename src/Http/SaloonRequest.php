9a4c3d39eeaf85119f4bbb9dcb845b4cedeac510
yurirnascimento <yuri.rsl@gmail.com>
2022-01-27 21:48:26 -0300

    Update SaloonRequest.php
    Can define a response class

--- src/Http/SaloonRequest.php
+++ src/Http/SaloonRequest.php
@@ -3,156 +3,187 @@
 namespace Sammyjo20\Saloon\Http;
 
 use ReflectionClass;
-use Sammyjo20\Saloon\Traits\CollectsData;
-use Sammyjo20\Saloon\Traits\SendsRequests;
-use Sammyjo20\Saloon\Traits\CollectsConfig;
-use Sammyjo20\Saloon\Traits\CollectsHeaders;
-use Sammyjo20\Saloon\Traits\CollectsHandlers;
-use Sammyjo20\Saloon\Traits\CollectsQueryParams;
-use Sammyjo20\Saloon\Traits\CollectsInterceptors;
-use Sammyjo20\Saloon\Interfaces\SaloonRequestInterface;
-use Sammyjo20\Saloon\Exceptions\SaloonMethodNotFoundException;
 use Sammyjo20\Saloon\Exceptions\SaloonInvalidConnectorException;
+use Sammyjo20\Saloon\Exceptions\SaloonInvalidResponseClassException;
+use Sammyjo20\Saloon\Exceptions\SaloonMethodNotFoundException;
+use Sammyjo20\Saloon\Interfaces\SaloonRequestInterface;
+use Sammyjo20\Saloon\Traits\CollectsConfig;
+use Sammyjo20\Saloon\Traits\CollectsData;
+use Sammyjo20\Saloon\Traits\CollectsHandlers;
+use Sammyjo20\Saloon\Traits\CollectsHeaders;
+use Sammyjo20\Saloon\Traits\CollectsInterceptors;
+use Sammyjo20\Saloon\Traits\CollectsQueryParams;
+use Sammyjo20\Saloon\Traits\SendsRequests;
 
 abstract class SaloonRequest implements SaloonRequestInterface
 {
-    use CollectsData,
-        CollectsQueryParams,
-        CollectsHeaders,
-        CollectsConfig,
-        CollectsHandlers,
-        CollectsInterceptors,
-        SendsRequests;
+	use CollectsData,
+		CollectsQueryParams,
+		CollectsHeaders,
+		CollectsConfig,
+		CollectsHandlers,
+		CollectsInterceptors,
+		SendsRequests;
 
-    /**
-     * Define the method that the request will use.
-     *
-     * @var string|null
-     */
-    protected ?string $method = null;
+	/**
+	 * Define the method that the request will use.
+	 *
+	 * @var string|null
+	 */
+	protected ?string $method = null;
 
-    /**
-     * The connector.
-     *
-     * @var string|null
-     */
-    protected ?string $connector = null;
+	/**
+	 * Define a custom response that the request will return.
+	 *
+	 * @var string|null
+	 */
+//    protected ?string $response;
 
-    /**
-     * The instantiated connector instance.
-     *
-     * @var SaloonConnector|null
-     */
-    private ?SaloonConnector $loadedConnector = null;
+	/**
+	 * The connector.
+	 *
+	 * @var string|null
+	 */
+	protected ?string $connector = null;
 
-    /**
-     * Define anything to be added to the request.
-     *
-     * @return void
-     */
-    public function boot(): void
-    {
-        // TODO: Implement boot() method.
-    }
+	/**
+	 * The instantiated connector instance.
+	 *
+	 * @var SaloonConnector|null
+	 */
+	private ?SaloonConnector $loadedConnector = null;
 
-    /**
-     * Get the method the class is using.
-     *
-     * @return string|null
-     */
-    public function getMethod(): ?string
-    {
-        if (empty($this->method)) {
-            return null;
-        }
+	/**
+	 * Define anything to be added to the request.
+	 *
+	 * @return void
+	 */
+	public function boot(): void
+	{
+		// TODO: Implement boot() method.
+	}
 
-        return $this->method;
-    }
+	/**
+	 * Get the method the class is using.
+	 *
+	 * @return string|null
+	 */
+	public function getMethod(): ?string
+	{
+		if(empty($this->method)) {
+			return null;
+		}
 
-    /**
-     * Boot the connector
-     *
-     * @return void
-     * @throws SaloonInvalidConnectorException
-     */
-    private function bootConnector(): void
-    {
-        if (empty($this->connector) || ! class_exists($this->connector)) {
-            throw new SaloonInvalidConnectorException;
-        }
+		return $this->method;
+	}
 
-        $isValidRequest = (new ReflectionClass($this->connector))->isSubclassOf(SaloonConnector::class);
+	/**
+	 * @return string
+	 * @throws \ReflectionException
+	 * @throws \Sammyjo20\Saloon\Exceptions\SaloonInvalidConnectorException
+	 * @throws \Sammyjo20\Saloon\Exceptions\SaloonInvalidResponseClassException
+	 */
+	public function getResponseClass(): string
+	{
+		$response = $this->response ?? SaloonResponse::class;
 
-        if (! $isValidRequest) {
-            throw new SaloonInvalidConnectorException;
-        }
+		if(!class_exists($response)) {
+			throw new SaloonInvalidResponseClassException;
+		}
 
-        $this->loadedConnector = new $this->connector;
-    }
+		$isValidResponse = (new ReflectionClass($response))->isSubclassOf(SaloonResponse::class);
 
-    /**
-     * Get the connector instance. If it hasn't yet been booted, we will boot it up.
-     *
-     * @return SaloonConnector
-     * @throws SaloonInvalidConnectorException
-     */
-    public function getConnector(): SaloonConnector
-    {
-        if (! $this->loadedConnector instanceof SaloonConnector) {
-            $this->bootConnector();
-        }
+		if(!$isValidResponse) {
+			throw new SaloonInvalidResponseClassException;
+		}
 
-        return $this->loadedConnector;
-    }
+		return $response;
+	}
 
-    /**
-     * Build up the final request URL.
-     *
-     * @return string
-     * @throws SaloonInvalidConnectorException
-     */
-    public function getFullRequestUrl(): string
-    {
-        $requestEndpoint = $this->defineEndpoint();
+	/**
+	 * Boot the connector
+	 *
+	 * @return void
+	 * @throws SaloonInvalidConnectorException
+	 */
+	private function bootConnector(): void
+	{
+		if(empty($this->connector) || !class_exists($this->connector)) {
+			throw new SaloonInvalidConnectorException;
+		}
 
-        if ($requestEndpoint !== '/') {
-            $requestEndpoint = ltrim($requestEndpoint, '/ ');
-        }
+		$isValidRequest = (new ReflectionClass($this->connector))->isSubclassOf(SaloonConnector::class);
 
-        $requiresTrailingSlash = ! empty($requestEndpoint) && $requestEndpoint !== '/';
+		if(!$isValidRequest) {
+			throw new SaloonInvalidConnectorException;
+		}
 
-        $baseEndpoint = rtrim($this->getConnector()->defineBaseUrl(), '/ ');
-        $baseEndpoint = $requiresTrailingSlash ? $baseEndpoint . '/' : $baseEndpoint;
+		$this->loadedConnector = new $this->connector;
+	}
 
-        return $baseEndpoint . $requestEndpoint;
-    }
+	/**
+	 * Get the connector instance. If it hasn't yet been booted, we will boot it up.
+	 *
+	 * @return SaloonConnector
+	 * @throws SaloonInvalidConnectorException
+	 */
+	public function getConnector(): SaloonConnector
+	{
+		if(!$this->loadedConnector instanceof SaloonConnector) {
+			$this->bootConnector();
+		}
 
-    /**
-     * Check if a trait exists on the connector.
-     *
-     * @param string $trait
-     * @return bool
-     * @throws SaloonInvalidConnectorException
-     */
-    public function traitExistsOnConnector(string $trait): bool
-    {
-        return array_key_exists($trait, class_uses($this->getConnector()));
-    }
+		return $this->loadedConnector;
+	}
 
-    /**
-     * Dynamically proxy other methods to the connector.
-     *
-     * @param $method
-     * @param $parameters
-     * @return mixed
-     * @throws SaloonMethodNotFoundException
-     */
-    public function __call($method, $parameters)
-    {
-        if (method_exists($this->getConnector(), $method) === false) {
-            throw new SaloonMethodNotFoundException($method, $this->getConnector());
-        }
+	/**
+	 * Build up the final request URL.
+	 *
+	 * @return string
+	 * @throws SaloonInvalidConnectorException
+	 */
+	public function getFullRequestUrl(): string
+	{
+		$requestEndpoint = $this->defineEndpoint();
 
-        return $this->getConnector()->{$method}(...$parameters);
-    }
+		if($requestEndpoint !== '/') {
+			$requestEndpoint = ltrim($requestEndpoint, '/ ');
+		}
+
+		$requiresTrailingSlash = !empty($requestEndpoint) && $requestEndpoint !== '/';
+
+		$baseEndpoint = rtrim($this->getConnector()->defineBaseUrl(), '/ ');
+		$baseEndpoint = $requiresTrailingSlash ? $baseEndpoint . '/' : $baseEndpoint;
+
+		return $baseEndpoint . $requestEndpoint;
+	}
+
+	/**
+	 * Check if a trait exists on the connector.
+	 *
+	 * @param string $trait
+	 * @return bool
+	 * @throws SaloonInvalidConnectorException
+	 */
+	public function traitExistsOnConnector(string $trait): bool
+	{
+		return array_key_exists($trait, class_uses($this->getConnector()));
+	}
+
+	/**
+	 * Dynamically proxy other methods to the connector.
+	 *
+	 * @param $method
+	 * @param $parameters
+	 * @return mixed
+	 * @throws SaloonMethodNotFoundException
+	 */
+	public function __call($method, $parameters)
+	{
+		if(method_exists($this->getConnector(), $method) === false) {
+			throw new SaloonMethodNotFoundException($method, $this->getConnector());
+		}
+
+		return $this->getConnector()->{$method}(...$parameters);
+	}
 }
