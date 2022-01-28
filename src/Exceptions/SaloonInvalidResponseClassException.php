0e2b01bd19a97f87dcaaad91104061465f477407
yurirnascimento <yuri.rsl@gmail.com>
2022-01-27 21:46:32 -0300

    Create SaloonInvalidResponseClassException.php

---
+++ src/Exceptions/SaloonInvalidResponseClassException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sammyjo20\Saloon\Exceptions;
+
+use Exception;
+
+class SaloonInvalidResponseClassException extends Exception
+{
+	public function __construct()
+	{
+		parent::__construct('The provided response is not a valid. The class must also extend SaloonResponse.');
+	}
+}
