9f05a6b13e0e219311deb7ed6186a9a874fb9a84
yurirnascimento <yuri.rsl@gmail.com>
2022-01-28 12:23:50 -0300

    Update HasResponseClass.php
    Change to implement getResponseClass and be used in Connector or Request.

--- src/Traits/Features/HasResponseClass.php
+++ src/Traits/Features/HasResponseClass.php
@@ -2,16 +2,42 @@
 
 namespace Sammyjo20\Saloon\Traits\Features;
 
+use Sammyjo20\Saloon\Exceptions\SaloonInvalidResponseClassException;
 use Sammyjo20\Saloon\Http\SaloonResponse;
+use \ReflectionClass;
 
 trait HasResponseClass
 {
 
 	protected ?string $response = null;
 
+	/**
+	 * Get the response class
+	 *
+	 * @return string
+	 * @throws \ReflectionException
+	 * @throws SaloonInvalidResponseClassException
+	 */
 	public function getResponseClass() : string
 	{
-		return $this->response ?? SaloonResponse::class;
+		$response = $this->response;
+
+		if(!$response) {
+			$response = method_exists($this, "getConnector") ? $this->getConnector()->getResponseClass() : SaloonResponse::class;
+		}
+
+		if(!class_exists($response)) {
+			throw new SaloonInvalidResponseClassException;
+		}
+
+		$isValidResponse = $response == SaloonResponse::class
+			|| (new ReflectionClass($response))->isSubclassOf(SaloonResponse::class);
+
+		if(!$isValidResponse) {
+			throw new SaloonInvalidResponseClassException;
+		}
+
+		return $response;
 	}
 
 }
