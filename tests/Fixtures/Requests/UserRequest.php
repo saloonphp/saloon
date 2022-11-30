2d69d29f9dfb3ef5a69bb06fef28ab62ce8f8589
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2022-11-30 21:01:09 +0000

    wip

--- tests/Fixtures/Requests/UserRequest.php
+++ tests/Fixtures/Requests/UserRequest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Saloon\Tests\Fixtures\Requests;
 
-use Saloon\Http\Request;
+use Saloon\Http\Request;    
 use Saloon\Http\SoloRequest;
 
 class UserRequest extends SoloRequest
