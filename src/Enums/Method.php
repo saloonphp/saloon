17885cc5e61688b3df247ffc0253aac2a401dc34
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2022-12-11 19:14:23 +0000

    Added additional methods

--- src/Enums/Method.php
+++ src/Enums/Method.php
@@ -7,9 +7,12 @@ namespace Saloon\Enums;
 enum Method: string
 {
     case GET = 'GET';
+    case HEAD = 'HEAD';
     case POST = 'POST';
     case PUT = 'PUT';
     case PATCH = 'PATCH';
     case DELETE = 'DELETE';
     case OPTIONS = 'OPTIONS';
+    case CONNECT = 'CONNECT';
+    case TRACE = 'TRACE';
 }
