e4dae533175b00c8cc3ab992d90ba630573f77c0
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2022-12-11 17:27:09 +0000

    Fixed conflicts

--- tests/Unit/StorageTest.php
+++ tests/Unit/StorageTest.php
@@ -55,12 +55,8 @@ test('it will create a file with nested folders', function () {
     $filesystem->createDirectory('/');
 
     $path = 'some' . DIRECTORY_SEPARATOR . 'other' . DIRECTORY_SEPARATOR . 'directories' . DIRECTORY_SEPARATOR . 'example.txt';
-
-<<<<<<< HEAD
-    $path = 'some' . DIRECTORY_SEPARATOR . 'other' . DIRECTORY_SEPARATOR . 'directories' . DIRECTORY_SEPARATOR . 'example.txt';
-=======
+    
     $storage = new Storage('tests' . DIRECTORY_SEPARATOR . 'Fixtures' . DIRECTORY_SEPARATOR . 'Saloon');
->>>>>>> v1
 
     expect($storage->exists($path))->toBeFalse();
 
