9db1d6d1b68b52a6c5e76b26ed3ccc4e7e5f3d99
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2023-02-06 18:10:04 +0000

    Added sender resolver for frameworks

--- src/Helpers/Config.php
+++ src/Helpers/Config.php
@@ -32,6 +32,13 @@ final class Config
     private static string $defaultSender = GuzzleSender::class;
 
     /**
+     * Sender resolver
+     *
+     * @var callable|null
+     */
+    private static mixed $senderResolver = null;
+
+    /**
      * Update global middleware
      *
      * @return \Saloon\Contracts\MiddlewarePipeline
@@ -42,13 +49,26 @@ final class Config
     }
 
     /**
+     * Write a custom sender resolver
+     *
+     * @param callable|null $senderResolver
+     * @return void
+     */
+    public static function resolveSenderWith(?callable $senderResolver): void
+    {
+        self::$senderResolver = $senderResolver;
+    }
+
+    /**
      * Create a new default sender
      *
      * @return \Saloon\Contracts\Sender
      */
     public static function getDefaultSender(): Sender
     {
-        return new self::$defaultSender;
+        $senderResolver = self::$senderResolver;
+
+        return is_callable($senderResolver) ? $senderResolver() : new self::$defaultSender;
     }
 
     /**
