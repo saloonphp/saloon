b52dd3e814bc1842af6ba6f3365ccd23a258fe6b
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2023-02-26 18:13:00 +0000

    wip

---
+++ src/Debugging/Drivers/FileDebugger.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Saloon\Debugging\Drivers;
+
+use InvalidArgumentException;
+use Saloon\Debugging\DebugData;
+
+class FileDebugger extends DebuggingDriver
+{
+    /**
+     * Resource
+     *
+     * @var resource $resource
+     */
+    protected mixed $resource;
+
+    /**
+     * Constructor
+     *
+     * @param resource $resource
+     */
+    public function __construct(mixed $resource)
+    {
+        if (! is_resource($resource)) {
+            throw new InvalidArgumentException('Invalid value for argument `$resource`. The value must be a resource like fopen.');
+        }
+
+        $this->resource = $resource;
+    }
+
+    /**
+     * @return string
+     */
+    public function name(): string
+    {
+        return 'file';
+    }
+
+    /**
+     * @param \Saloon\Debugging\DebugData $data
+     *
+     * @return void
+     * @throws \JsonException
+     */
+    public function send(DebugData $data): void
+    {
+        $message = $data->wasNotSent() ? 'Saloon Request' : 'Saloon Response';
+        $encoded = json_encode($this->formatData($data, true), JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT);
+
+        fwrite($this->resource, sprintf('%s:%s%s %s', $message, PHP_EOL, $encoded, PHP_EOL));
+    }
+}
--- tests/Feature/Debugging/DebuggerDriverTest.php
+++ tests/Feature/Debugging/DebuggerDriverTest.php
@@ -3,3 +3,23 @@
 declare(strict_types=1);
 
 // TODO: Test each debugging driver works
+
+use Saloon\Debugging\Drivers\FileDebugger;
+use Saloon\Tests\Fixtures\Connectors\TestConnector;
+use Saloon\Tests\Fixtures\Requests\UserRequest;
+
+test('you can debug the file driver', function () {
+    $connector = new TestConnector;
+
+    $connector->debug()
+        ->showRequestAndResponse()
+        ->usingDriver(new FileDebugger(
+            resource: fopen('tests/Fixtures/Saloon/Testing/debug.txt', 'wb')
+        ));
+
+    $connector->send(new UserRequest);
+});
+
+test('you can debug the flysystem driver', function () {
+    //
+});
