67938443ef961c116f7cf7c5f2cf64c4dfc49498
Sam Carré <29132017+Sammyjo20@users.noreply.github.com>
2023-03-08 23:26:28 +0000

    Update src/Contracts/HasDebugging.php
    Co-authored-by: Juse Less <me@juseless.dev>

--- src/Contracts/HasDebugging.php
+++ src/Contracts/HasDebugging.php
@@ -11,7 +11,7 @@ interface HasDebugging
     /**
      * Retrieve the debugger
      *
-     * @param callable|null $callback
+     * @param callable(\Saloon\Debugging\Debugger): (void)|null $callback
      * @return \Saloon\Debugging\Debugger
      */
     public function debug(?callable $callback = null): Debugger;
