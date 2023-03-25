22ff348bc4bbcc7264f20d7674276a4456aa1f10
Sammyjo20 <Sammyjo20@users.noreply.github.com>
2023-03-25 16:41:56 +0000

    🪄 Code Style Fixes

--- tests/Unit/PendingRequestTest.php
+++ tests/Unit/PendingRequestTest.php
@@ -1,17 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 use Saloon\Enums\Method;
+use Saloon\Http\PendingRequest;
 use Saloon\Http\Faking\MockClient;
 use Saloon\Http\Faking\MockResponse;
-use Saloon\Http\PendingRequest;
-use Saloon\Tests\Fixtures\Connectors\TestConnector;
 use Saloon\Tests\Fixtures\Requests\UserRequest;
+use Saloon\Tests\Fixtures\Connectors\TestConnector;
 
 test('you can overwrite the url and the method of the pending request', function () {
     $connector = new TestConnector;
 
     $connector->withMockClient(new MockClient([
-        new MockResponse(['name' => 'Sam'])
+        new MockResponse(['name' => 'Sam']),
     ]));
 
     $connector->middleware()->onRequest(function (PendingRequest $pendingRequest) {
