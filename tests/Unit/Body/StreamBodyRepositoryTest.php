47a6934996be3a549540e54cd8ff25208ae7cd49
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2023-06-15 16:56:03 +0100

    Fixed pstan issues

--- tests/Unit/Body/StreamBodyRepositoryTest.php
+++ tests/Unit/Body/StreamBodyRepositoryTest.php
@@ -13,40 +13,40 @@ test('the store is empty by default', function () {
 
 
 test('the store can have a default stream provided', function () {
-    $temp = tmpfile();
+    $resource = tmpfile();
 
-    $body = new StreamBodyRepository($temp);
+    $body = new StreamBodyRepository($resource);
 
-    expect($body->all())->toEqual(Utils::streamFor($temp));
+    expect($body->all())->toEqual(Utils::streamFor($resource));
 });
 
 test('you can set it', function () {
-    $tempA = fopen('php://memory', 'rw+');
-    fwrite($tempA, 'Howdy');
+    $resourceA = fopen('php://memory', 'rw+');
+    fwrite($resourceA, 'Howdy');
 
-    $tempB = fopen('php://memory', 'rw+');
-    fwrite($tempB, 'Yeehaw');
+    $resourceB = fopen('php://memory', 'rw+');
+    fwrite($resourceB, 'Yeehaw');
 
-    $body = new StreamBodyRepository($tempA);
+    $body = new StreamBodyRepository($resourceA);
 
-    $body->set($tempB);
+    $body->set($resourceB);
 
-    expect($body->all())->toEqual(Utils::streamFor($tempB));
+    expect($body->all())->toEqual(Utils::streamFor($resourceB));
 });
 
 test('you can conditionally set on the store', function () {
     $body = new StreamBodyRepository();
 
-    $tempA = fopen('php://memory', 'rw+');
-    fwrite($tempA, 'Howdy');
+    $resourceA = fopen('php://memory', 'rw+');
+    fwrite($resourceA, 'Howdy');
 
-    $tempB = fopen('php://memory', 'rw+');
-    fwrite($tempB, 'Yeehaw');
+    $resourceB = fopen('php://memory', 'rw+');
+    fwrite($resourceB, 'Yeehaw');
 
-    $body->when(true, fn (StreamBodyRepository $body) => $body->set($tempA));
-    $body->when(false, fn (StreamBodyRepository $body) => $body->set($tempB));
+    $body->when(true, fn (StreamBodyRepository $body) => $body->set($resourceA));
+    $body->when(false, fn (StreamBodyRepository $body) => $body->set($resourceB));
 
-    expect($body->all())->toEqual(Utils::streamFor($tempA));
+    expect($body->all())->toEqual(Utils::streamFor($resourceA));
 });
 
 test('you can check if the store is empty or not', function () {
@@ -93,4 +93,16 @@ test('it allows null values', function () {
 
     expect($body->all())->toBeNull();
     expect($body->isEmpty())->toBeTrue();
+});
+
+test('the stream is rewound after converting to a string', function () {
+    $resource = fopen('php://memory', 'rw+');
+    fwrite($resource, 'Howdy');
+    rewind($resource);
+
+    $body = new StreamBodyRepository($resource);
+    $stream = $body->get();
+
+    expect((string)$body)->toEqual('Howdy');
+    expect($stream->getContents())->toEqual('Howdy');
 });
