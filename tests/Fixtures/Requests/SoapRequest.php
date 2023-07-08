45c1a735c0172c7a5806b6253d3b825d247ba4be
Fatih Aydın <fatihaydin@temizlikyolda.com>
2023-07-08 23:09:02 +0300

    test: Added `defaultConfig` and `defaultHeaders` methods to `SoapRequest`

--- tests/Fixtures/Requests/SoapRequest.php
+++ tests/Fixtures/Requests/SoapRequest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Saloon\Tests\Fixtures\Requests;
 
+use SoapHeader;
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
 
@@ -21,10 +22,46 @@ class SoapRequest extends Request
         return 'FahrenheitToCelsius';
     }
 
+    /**
+     * Default Query Parameters
+     *
+     * @return array<string, mixed>
+     */
     protected function defaultQuery(): array
     {
         return [
             'Fahrenheit' => $this->fahrenheit,
+        ];
+    }
+
+    /**
+     * Default Config
+     *
+     * @return array<string, mixed>
+     */
+    protected function defaultConfig(): array
+    {
+        return [
+            'connection_timeout' => 10, // in seconds,
+            'stream_context' => stream_context_create([
+                'http' => [
+                    'user_agent' => 'Test',
+                ],
+            ]),
+        ];
+    }
+
+    /**
+     * Define the soap headers that will be applied in every request.
+     *
+     * @return array<string, mixed>
+     */
+    protected function defaultHeaders(): array
+    {
+        return [
+            new SoapHeader(namespace: 'namespace1', name: 'name1', data: 'data1'),
+            'namespace2' => ['name2','data2', false, null],
+            'namespace3' => 'name3',
         ];
     }
 
