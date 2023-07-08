f89836216f9ae7deba875b3cecdf2d60aa327440
Fatih Aydın <fatihaydin@temizlikyolda.com>
2023-07-08 23:09:23 +0300

    test: Added soap test in `RequestTest`

--- tests/Feature/RequestTest.php
+++ tests/Feature/RequestTest.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 use Saloon\Http\PendingRequest;
 use Saloon\Http\Responses\Response;
 use Saloon\Http\Senders\GuzzleSender;
+use Saloon\Http\Responses\SoapResponse;
+use Saloon\Tests\Fixtures\Requests\SoapRequest;
 use Saloon\Tests\Fixtures\Requests\UserRequest;
 use Saloon\Tests\Fixtures\Requests\ErrorRequest;
 use Saloon\Tests\Fixtures\Connectors\TestConnector;
+use Saloon\Tests\Fixtures\Connectors\SoapClientConnector;
 use Saloon\Tests\Fixtures\Requests\HasConnectorUserRequest;
 
 test('a request can be made successfully', function () {
@@ -25,6 +28,22 @@ test('a request can be made successfully', function () {
         'name' => 'Sammyjo20',
         'actual_name' => 'Sam',
         'twitter' => '@carre_sam',
+    ]);
+});
+
+test('a soap request can be made successfully', function () {
+    $connector = new SoapClientConnector();
+    $response = $connector->send(new SoapRequest(fahrenheit: 1));
+
+    $data = $response->json();
+
+    expect($response->getPendingRequest()->isAsynchronous())->toBeFalse();
+    expect($response)->toBeInstanceOf(SoapResponse::class);
+    expect($response->isMocked())->toBeFalse();
+    expect($response->status())->toEqual(200);
+
+    expect($data)->toEqual([
+        'FahrenheitToCelsiusResult' => '-17.2222222222222',
     ]);
 });
 
