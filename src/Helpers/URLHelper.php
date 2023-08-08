41e1b57a66119e7d55dc74b7625b71d3ea61af21
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2023-08-08 18:25:15 +0100

    Revert

--- src/Helpers/URLHelper.php
+++ src/Helpers/URLHelper.php
@@ -15,7 +15,7 @@ class URLHelper
      */
     public static function matches(string $pattern, string $value): bool
     {
-        return StringHelper::matchesPattern(StringHelper::start($pattern, '*'), $value);
+        return StringHelpers::matchesPattern(StringHelpers::start($pattern, '*'), $value);
     }
 
     /**
