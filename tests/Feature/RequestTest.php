a5d5d24a52e60fb95d656d6612f79d5efa1cd012
Enzo Innocenzi <enzo@innocenzi.dev>
2023-10-02 20:27:17 +0200

    test: move `SoloRequestTest` tests in `RequestTest`

--- tests/Feature/RequestTest.php
+++ tests/Feature/RequestTest.php
@@ -8,6 +8,8 @@ use Saloon\Http\Senders\GuzzleSender;
 use Saloon\Tests\Fixtures\Requests\UserRequest;
 use Saloon\Tests\Fixtures\Requests\ErrorRequest;
 use Saloon\Tests\Fixtures\Connectors\TestConnector;
+use Saloon\Tests\Fixtures\Requests\SoloUserRequest;
+use Saloon\Tests\Fixtures\Requests\SoloErrorRequest;
 use Saloon\Tests\Fixtures\Requests\HasConnectorUserRequest;
 
 test('a request can be made successfully', function () {
@@ -56,4 +58,60 @@ test('a request with HasConnector can be sent individually', function () {
         'actual_name' => 'Sam',
         'twitter' => '@carre_sam',
     ]);
+});
+
+
+test('a solo request can be sent synchronously', function () {
+    $request = new SoloUserRequest;
+    $response = $request->send();
+
+    $data = $response->json();
+
+    expect($response)->toBeInstanceOf(Response::class);
+    expect($response->isMocked())->toBeFalse();
+    expect($response->status())->toEqual(200);
+
+    expect($data)->toEqual([
+        'name' => 'Sammyjo20',
+        'actual_name' => 'Sam',
+        'twitter' => '@carre_sam',
+    ]);
+});
+
+test('a synchronous solo request can handle an exception property', function () {
+    $request = new SoloErrorRequest;
+    $response = $request->send();
+
+    expect($response->isMocked())->toBeFalse();
+    expect($response->status())->toEqual(500);
+});
+
+test('a solo request can be sent asynchronously', function () {
+    $request = new SoloUserRequest;
+    $promise = $request->sendAsync();
+
+    expect($promise)->toBeInstanceOf(PromiseInterface::class);
+
+    $response = $promise->wait();
+
+    $data = $response->json();
+
+    expect($response)->toBeInstanceOf(Response::class);
+    expect($response->isMocked())->toBeFalse();
+    expect($response->status())->toEqual(200);
+
+    expect($data)->toEqual([
+        'name' => 'Sammyjo20',
+        'actual_name' => 'Sam',
+        'twitter' => '@carre_sam',
+    ]);
+});
+
+test('a asynchronous solo request can handle an exception property', function () {
+    $request = new SoloErrorRequest();
+    $promise = $request->sendAsync();
+
+    $this->expectException(RequestException::class);
+
+    $promise->wait();
 });
