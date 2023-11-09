8cf5e39385797a9714615f50b4d765b59592a766
Bob Mulder <info@madebybob.nl>
2023-11-09 09:19:04 +0100

    Add extra test for query parameters at EndpointEndsWithCheckTest

--- tests/Unit/Testing/Checks/EndpointEndsWithCheckTest.php
+++ tests/Unit/Testing/Checks/EndpointEndsWithCheckTest.php
@@ -6,6 +6,7 @@ namespace Saloon\Tests\Unit\Testing\Checks;
 
 use Saloon\Tests\Fixtures\Requests\HasJsonBodyRequest;
 use Saloon\Helpers\Testing\Checks\EndpointEndsWithCheck;
+use Saloon\Tests\Fixtures\Requests\QueryParameterRequest;
 
 test('you can validate if a given request ends with the expected url', function () {
     $actual = connector()->createPendingRequest(new HasJsonBodyRequest());
@@ -28,4 +29,15 @@ test('you can validate if a given request does not end with the expected url', f
 
     expect($check->valid())->toBeFalse();
     expect($check->message())->toEqual('The url did not end with \'/users\'');
+});
+
+test('you can pass the rule when query parameters are used', function () {
+    $actual = connector()->createPendingRequest(new QueryParameterRequest());
+
+    $check = EndpointEndsWithCheck::make(
+        $actual,
+        '/user',
+    );
+
+    expect($check->valid())->toBeTrue();
 });
