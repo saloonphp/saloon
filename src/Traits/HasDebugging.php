96ff118f4f5cf0c12f8dc8f4548d4c9626c2bd6f
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2024-03-19 18:02:57 +0000

    Removed helper, added comments

--- src/Traits/HasDebugging.php
+++ src/Traits/HasDebugging.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace Saloon\Traits;
 
-use Saloon\Http\Request;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Saloon\Http\Response;
-use function Saloon\debug;
+use Symfony\Component\VarDumper\VarDumper;
 use Saloon\Enums\PipeOrder;
 use Saloon\Http\PendingRequest;
 
@@ -22,9 +23,32 @@ trait HasDebugging
      */
     public function debugRequest(?callable $onRequest = null, bool $die = false): static
     {
-        if (is_null($onRequest)) {
-            return debug($this, response: false, die: $die);
-        }
+        // When the user has not specified a callable to debug with, we will use this default
+        // debugging driver. This will use symfony/var-dumper to display a nice output to
+        // the user's screen of the request.
+
+        $onRequest ??= static function (PendingRequest $pendingRequest, RequestInterface $psrRequest) {
+            $headers = [];
+
+            foreach ($psrRequest->getHeaders() as $headerName => $value) {
+                $headers[$headerName] = implode(';', $value);
+            }
+
+            $className = explode('\\', $pendingRequest->getRequest()::class);
+            $label = end($className);
+
+            VarDumper::dump([
+                'connector' => $pendingRequest->getConnector()::class,
+                'request' => $pendingRequest->getRequest()::class,
+                'method' => $psrRequest->getMethod(),
+                'uri' => (string)$psrRequest->getUri(),
+                'headers' => $headers,
+                'body' => (string)$psrRequest->getBody(),
+            ], 'Saloon Request (' . $label . ') ->');
+        };
+
+        // Register the middleware - we will use PipeOrder::FIRST to ensure that the response
+        // is shown before it is modified by the user's middleware.
 
         $this->middleware()->onRequest(
             callable: static function (PendingRequest $pendingRequest) use ($onRequest, $die): void {
@@ -50,9 +74,29 @@ trait HasDebugging
      */
     public function debugResponse(?callable $onResponse = null, bool $die = false): static
     {
-        if (is_null($onResponse)) {
-            return debug($this, request: false, die: $die);
-        }
+        // When the user has not specified a callable to debug with, we will use this default
+        // debugging driver. This will use symfony/var-dumper to display a nice output to
+        // the user's screen of the response.
+
+        $onResponse ??= static function (Response $response, ResponseInterface $psrResponse) {
+            $headers = [];
+
+            foreach ($psrResponse->getHeaders() as $headerName => $value) {
+                $headers[$headerName] = implode(';', $value);
+            }
+
+            $className = explode('\\', $response->getRequest()::class);
+            $label = end($className);
+
+            VarDumper::dump([
+                'status' => $response->status(),
+                'headers' => $headers,
+                'body' => $response->body(),
+            ], 'Saloon Response (' . $label . ') ->');
+        };
+
+        // Register the middleware - we will use PipeOrder::FIRST to ensure that the response
+        // is shown before it is modified by the user's middleware.
 
         $this->middleware()->onResponse(
             callable: static function (Response $response) use ($onResponse, $die): void {
@@ -69,14 +113,19 @@ trait HasDebugging
     }
 
     /**
-     * Dump a request before it is sent and the response.
+     * Dump a pretty output of the request and response.
      *
-     * Note that anything added by the sender will not be reflected here.
+     * This is useful if you would like to see the request right before it is sent
+     * to inspect the body and URI to ensure it is correct. You can also inspect
+     * the raw response as it comes back.
+     *
+     * Note that any changes made to the PSR request by the sender will not be
+     * reflected by this output.
      *
      * Requires symfony/var-dumper
      */
     public function debug(bool $die = false): static
     {
-        return debug($this, die: $die);
+        return $this->debugRequest()->debugResponse(die: $die);
     }
 }
