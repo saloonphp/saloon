21c548c67d4185e634462bba0d511960557f6132
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2025-05-02 18:18:26 +0100

    Fixed PHPStan with PHP 8.4

--- src/Http/Faking/Fixture.php
+++ src/Http/Faking/Fixture.php
@@ -52,7 +52,7 @@ class Fixture
     /**
      * Constructor
      */
-    public function __construct(string $name = '', ?Storage $storage = null, ArrayStoreContract $context = null)
+    public function __construct(string $name = '', ?Storage $storage = null, ?ArrayStoreContract $context = null)
     {
         $this->name = $name;
         $this->storage = $storage ?? new Storage(MockConfig::getFixturePath(), true);
