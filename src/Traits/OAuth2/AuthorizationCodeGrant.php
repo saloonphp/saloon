ce5c9060d187a29ddadf9df6a715e2195e49a1b5
Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>
2025-11-20 21:22:33 +0000

    Revert "🪄 Code Style Fixes"
    This reverts commit 68e068a243a799c8db339acf72b5975a8ed9208b.

--- src/Traits/OAuth2/AuthorizationCodeGrant.php
+++ src/Traits/OAuth2/AuthorizationCodeGrant.php
@@ -58,7 +58,7 @@ trait AuthorizationCodeGrant
         ]);
 
         $query = http_build_query($queryParameters, '', '&', PHP_QUERY_RFC3986);
-        $query = mb_trim($query, '?&');
+        $query = trim($query, '?&');
 
         $url = URLHelper::join($this->resolveBaseUrl(), $config->getAuthorizeEndpoint());
 
