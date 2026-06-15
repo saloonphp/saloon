e67e38ccc614533774ee38b1f1275d62004cf5f1
Paulo Frois <froispaulo@outlook.com>
2026-06-15 12:02:16 -0300

    Remove unused import

--- tests/Fixtures/Requests/NullHeaderRequest.php
+++ tests/Fixtures/Requests/NullHeaderRequest.php
@@ -6,7 +6,6 @@ namespace Saloon\Tests\Fixtures\Requests;
 
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
-use Saloon\Tests\Fixtures\Connectors\HeaderConnector;
 
 class NullHeaderRequest extends Request
 {
