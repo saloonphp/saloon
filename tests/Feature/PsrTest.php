43d2c787d41ab3923a88b478b3b09d403e2f51c6
Paulo Frois <froispaulo@outlook.com>
2026-09-18 18:01:46 -0300

    [4.x] Fix: Null header deprecated notice for PSR requests (#554)
    * Fix: Null header deprecated notice for PSR requests
    
    Notice: Since guzzlehttp/psr7 2.11: Passing null to MessageInterface::withHeader() is deprecated; guzzlehttp/psr7 3.0 requires string|string[].
    
    * Add tests to null-header to string conversion
    
    * Remove unused import
    
    * Remove debug
    
    * Fix typos and cleanup
    
    ---------
    
    Co-authored-by: Sammyjo20 <29132017+Sammyjo20@users.noreply.github.com>

--- tests/Feature/PsrTest.php
+++ tests/Feature/PsrTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 use GuzzleHttp\Psr7\Uri;
 use Saloon\Http\Faking\MockClient;
 use Saloon\Http\Faking\MockResponse;
+use Saloon\Tests\Fixtures\Connectors\TestConnector;
+use Saloon\Tests\Fixtures\Requests\NullHeaderRequest;
 use Saloon\Tests\Fixtures\Requests\ModifiedPsrUserRequest;
 use Saloon\Tests\Fixtures\Connectors\ModifiedPsrRequestConnector;
 
@@ -25,4 +27,21 @@ test('the connector and request can modify the psr request when it is created', 
     // The request will add the X-Howdy header
 
     expect($response->getPsrRequest()->getHeaders())->toHaveKey('X-Howdy', ['Yeehaw']);
+});
+
+test('The psr request headers must be converted to empty string', function () {
+    $mockClient = new MockClient([
+        MockResponse::make(headers: ['X-Null-Header' => null]),
+    ]);
+
+    $connector = new TestConnector();
+    $connector->withMockClient($mockClient);
+
+    $response = $connector->send(new NullHeaderRequest());
+
+    // The request will convert null to empty string
+    expect($response->getPsrRequest()->getHeader('X-Null-Header')[0])->toBe('');
+
+    // The response will convert null header to empty string
+    expect($response->headers()->get('X-Null-Header'))->toBe('');
 });
---
+++ tests/Fixtures/Requests/NullHeaderRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Saloon\Tests\Fixtures\Requests;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+
+class NullHeaderRequest extends Request
+{
+    /**
+     * Define the method that the request will use.
+     *
+     * @var string
+     */
+    protected Method $method = Method::GET;
+
+    /**
+     * Define the endpoint for the request.
+     */
+    public function resolveEndpoint(): string
+    {
+        return '/user';
+    }
+
+    protected function defaultHeaders(): array
+    {
+        return [
+            'X-Null-Header' => null,
+        ];
+    }
+}
